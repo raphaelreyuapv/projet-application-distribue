a2e719934cfaa58cec8a99624583c3d491a38c19
uapv1902691 <pierre.de-saxce-nahon@alumni.univ-avignon.fr>
2022-05-11 08:58:33 +0200

    Ajout de SpeechRecognition a MainActivity

--- app/src/main/java/projet/application/MainActivity.java
+++ app/src/main/java/projet/application/MainActivity.java
@@ -1,11 +1,22 @@
 package projet.application;
 
+import static android.speech.SpeechRecognizer.createOnDeviceSpeechRecognizer;
+import static android.speech.SpeechRecognizer.createSpeechRecognizer;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -18,15 +29,22 @@ import projet.application.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private Context context;
+    private static final String TAG = "MyActivity";
+    private View myView;
+    private boolean recording = false;
+    SpeechRecognizer speechRec = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        context = getApplicationContext();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -39,8 +57,37 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                if (speechRec==null && !SpeechRecognizer.isRecognitionAvailable(context)) {
+                    Snackbar.make(view, "Votre telephone ne permet pas la reconnaissance vocale", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else{
+                    if(speechRec==null){
+
+                        speechRec = SpeechRecognizer.createSpeechRecognizer(context);
+                        speechRec.setRecognitionListener(new listener());
+                        myView = view;
+
+                        String requiredPermission = Manifest.permission.RECORD_AUDIO;
+                        // If the user previously denied this permission then show a message explaining why
+                        // this permission is needed
+                        if (checkCallingOrSelfPermission(requiredPermission) == PackageManager.PERMISSION_DENIED) {
+                            requestPermissions(new String[]{requiredPermission}, 101);
+                        }
+                    }
+
+                    if(!recording) {
+                        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                        speechRec.startListening(intent);
+                    }
+                    else {
+
+                        speechRec.cancel();
+                    }
+
+                    recording = !recording;
+
+                }
             }
         });
     }
@@ -73,4 +120,63 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+    class listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+            Snackbar.make(myView, "Debut de la reconnaissance audio", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+            Log.d(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error)
+        {
+            Log.d(TAG,  "error " +  error);
+            Snackbar.make(myView, "error " + error, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+        public void onResults(Bundle results)
+        {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i);
+            }
+
+            Snackbar.make(myView, str, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+            Log.d(TAG, "onPartialResults");
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
+        }
+    }
+
+
+
 }
